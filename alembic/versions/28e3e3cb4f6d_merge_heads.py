13082bc920d1fa73964489839f53bb33264bb2c9
AbdulRauf Siddiqui <abdulrauf_99@outlook.com>
2025-09-08 12:00:31 +0500

    merged db

---
+++ alembic/versions/28e3e3cb4f6d_merge_heads.py
@@ -0,0 +1,24 @@
+"""Merge heads
+
+Revision ID: 28e3e3cb4f6d
+Revises: 85677eb93283, 9c49547213f6
+Create Date: 2025-09-08 07:00:02.443065+00:00
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '28e3e3cb4f6d'
+down_revision = ('85677eb93283', '9c49547213f6')
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
