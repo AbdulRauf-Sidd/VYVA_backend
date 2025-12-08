c5cd086b844e11548aba5e03ddcc5c9a0f6f272f
AbdulRauf Siddiqui <abdulrauf_99@outlook.com>
2025-12-08 17:22:45 +0500

    added user check in

--- alembic/env.py
+++ alembic/env.py
@@ -10,7 +10,7 @@ from alembic import context
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 # Import all models to ensure they are registered with SQLAlchemy
 # from models import Base
-from models import eleven_labs_batch_calls, eleven_labs_sessions, onboarding, organization, user, authentication
+from models import eleven_labs_batch_calls, eleven_labs_sessions, onboarding, organization, user, authentication, user_check_ins
 from core.config import settings
 from core.database import Base
 
