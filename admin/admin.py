cdad5d757a6e5ffd45c46c45f05de669806f1163
AbdulRauf Siddiqui <abdulrauf_99@outlook.com>
2025-12-23 15:17:30 +0500

    added onboarding in admin

--- admin/admin.py
+++ admin/admin.py
@@ -2,11 +2,12 @@ from sqladmin import Admin, ModelView
 from core.database import engine
 from models import User, Medication
 from models.organization import Organization
+from models.onboarding import OnboardingUser
+from models.onboarding import OnboardingLogs
 from admin.auth import AdminAuth
 from core.config import settings
 
 def setup_admin(app):
-    # admin = Admin(app, engine)
     admin = Admin(
         app,
         engine,
@@ -24,12 +25,14 @@ def setup_admin(app):
     class OrganizationAdmin(ModelView, model=Organization):
         column_list = "__all__"
 
-    # admin = Admin(
-    #     app,
-    #     engine,
-    #     authentication_backend=AdminAuth(secret_key=settings.SECRET_KEY),
-    # )
+    class OnboardingUserAdmin(ModelView, model=OnboardingUser):
+        column_list = "__all__"
+
+    class OnboardingLogsAdmin(ModelView, model=OnboardingLogs):
+        column_list = "__all__"
 
     admin.add_view(UserAdmin)
     admin.add_view(MedicationAdmin)
     admin.add_view(OrganizationAdmin)
+    admin.add_view(OnboardingUserAdmin)
+    admin.add_view(OnboardingLogsAdmin)
