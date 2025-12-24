5e3de04e91429eef869c0c690443cdca15441889
AbdulRauf Siddiqui <abdulrauf_99@outlook.com>
2025-12-24 18:19:30 +0500

    changed mcp instance structure

--- main.py
+++ main.py
@@ -43,6 +43,7 @@ from celery.app.control import Inspect
 from celery_app import celery_app
 from mcp_tools.mcp_instance import mcp
 from mem0 import MemoryClient
+from mcp_tools import user
 
 # Setup logging
 logger = setup_logging()
