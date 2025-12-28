69217b58a8dcfe9bc91b57c5d4b7d65b66a4c37a
AbdulRauf Siddiqui <abdulrauf_99@outlook.com>
2025-12-28 18:04:43 +0500

    added zamora in orgins

--- core/config.py
+++ core/config.py
@@ -41,6 +41,7 @@ class Settings(BaseSettings):
         "https://4f214c49a3b2.ngrok-free.app",
         "https://onboarding.vyva.io",
         "http://localhost:3000",
+        "https://zamora.vyva.io",
     ]
     
     # CORS
